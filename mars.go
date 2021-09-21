0081453844a3a970d0291501a1e072e92ac55d8d
Ferdi ÜNAL <ferdiunal@outlook.com>
2021-09-21 21:56:21 +0300

    New features added

--- mars.go
+++ mars.go
@@ -1,48 +1,52 @@
 package mars
 
 import (
-	"crypto/sha256"
-	"fmt"
+	"errors"
 
-	"github.com/ferdiunal/mars/models"
-	"github.com/gofiber/fiber/v2/utils"
 	"gorm.io/gorm"
 )
 
 type Mars struct {
-	db             *gorm.DB
-	plainTextToken string
+	db *gorm.DB
 }
 
-func (m *Mars) hash() string {
-	return fmt.Sprintf(
-		"%x",
-		sha256.Sum256(
-			[]byte(m.plainTextToken),
-		),
-	)
+type MarsInterface interface {
+	CreateToken(userId uint64, name string, abilities []string) *MarsToken
+	CheckToken(userId uint64, tokenId uint64, token string) (*PersonalAccessToken, error)
+	RevokeToken(userId uint64, tokenId uint64, token string) error
 }
 
-func (m *Mars) GeneratePlainTextToken() *Mars {
-	m.plainTextToken = utils.UUIDv4()
+func (m *Mars) CreateToken(userId uint64, name string, abilities []string) *MarsToken {
+	model := NewPersonalAccessToken(userId, name, abilities)
+	m.db.Create(model)
 
-	return m
+	return model.GetResult()
 }
 
-func (m *Mars) CreateToken(userId uint64, name string, abilities []string) string {
-	token := &models.PersonalAccessToken{
-		Name:      name,
-		Abilities: abilities,
-		Token:     m.hash(),
-		UserId:    userId,
+func (m *Mars) CheckToken(userId uint64, tokenId uint64, token string) (*PersonalAccessToken, error) {
+	model := &PersonalAccessToken{}
+	m.db.Where("id = ? and userId = ?", tokenId, userId).Find(model)
+
+	if ok := model.HashedToken() == token; ok {
+		return model, nil
 	}
 
-	m.db.Create(token)
-
-	return fmt.Sprintf("%v|%v", token.ID, m.plainTextToken)
+	return nil, errors.New("token not found")
 }
 
-func NewMars(db *gorm.DB) *Mars {
+func (m *Mars) RevokeToken(userId uint64, tokenId uint64, token string) error {
+	model, err := m.CheckToken(userId, tokenId, token)
+
+	if err != nil {
+		return err
+	}
+
+	m.db.Delete(model)
+
+	return nil
+}
+
+func NewMars(db *gorm.DB) MarsInterface {
 	return &Mars{
 		db: db,
 	}
