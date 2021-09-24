89d4e6447f4313841da92fe8cb5e08e56361b3ca
Ferdi ÜNAL <ferdiunal@outlook.com>
2021-09-24 23:01:09 +0300

    fix json column type

--- PersonalAccessToken.go
+++ PersonalAccessToken.go
@@ -7,22 +7,23 @@ import (
 	"time"
 
 	"github.com/ferdiunal/venus"
+	"gorm.io/datatypes"
 
 	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 type PersonalAccessToken struct {
-	ID         uint64       `gorm:"autoIncrement;primaryKey"`
-	UserId     uint64       `gorm:"foreignkey:id;references:user_id;constraint:OnUpdate:SET NULL,OnDelete:CASCADE;"`
-	Name       string       `gorm:"uniqueIndex;size:255"`
-	Token      string       `gorm:"uniqueIndex;size:64"`
-	Abilities  []string     `gorm:"type:json"`
-	LastUsedAt sql.NullTime `gorm:"type:timestamp;"`
-	ExpireAt   time.Time    `gorm:"type:timestamp;"`
-	CreatedAt  time.Time    `gorm:"type:timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP"`
-	UpdatedAt  time.Time    `gorm:"type:timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"`
-	DeletedAt  sql.NullTime `gorm:"type:timestamp;"`
+	ID         uint64         `gorm:"autoIncrement;primaryKey"`
+	UserId     uint64         `gorm:"foreignkey:id;references:user_id;constraint:OnUpdate:SET NULL,OnDelete:CASCADE;"`
+	Name       string         `gorm:"uniqueIndex;size:255"`
+	Token      string         `gorm:"uniqueIndex;size:64"`
+	Abilities  datatypes.JSON `gorm:"type:json"`
+	LastUsedAt sql.NullTime   `gorm:"type:timestamp;"`
+	ExpireAt   time.Time      `gorm:"type:timestamp;"`
+	CreatedAt  time.Time      `gorm:"type:timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP"`
+	UpdatedAt  time.Time      `gorm:"type:timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"`
+	DeletedAt  sql.NullTime   `gorm:"type:timestamp;"`
 }
 
 type MarsToken struct {
--- go.mod
+++ go.mod
@@ -12,4 +12,5 @@ require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.2 // indirect
 	github.com/speps/go-hashids/v2 v2.0.1 // indirect
+	gorm.io/datatypes v1.0.2 // indirect
 )
